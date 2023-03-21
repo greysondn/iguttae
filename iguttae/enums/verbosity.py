2bd3b627d69832c60c961a7342abc47c2f615555
J. 'Dorian Greyson' L <greysondn@gmail.com>
2023-03-21 05:03:03 -0400

    Port Verbosity Enum

---
+++ iguttae/enums/verbosity.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+'''
+Represents how verbose something should be
+'''
+class Verbosity(Enum):
+    # TODO: Add silent mode
+    SILENT          = 0
+    
+    # TODO: Add superbrief mode
+    SUPERBRIEF      = 250
+    
+    # TODO: Add brief mode
+    BRIEF           = 500
+    
+    VERBOSE         = 750
+    '''
+    Default mode.
+    
+    Show text every time we enter a place, show list of items,
+    don't show item descriptions or character descriptions
+    '''
+    
+    # TODO: Add Superverbose Mode
+    SUPERVERBOSE    = 1000
